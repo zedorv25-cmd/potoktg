a6ce8eccbfabe107228f5ad47d89d10dd2ad22c2
zedorv25-cmd <zedorv25@gmail.com>
2026-06-07 14:02:03 +0500

    Update LaunchActivity.java

--- TMessagesProj/src/main/java/org/telegram/ui/LaunchActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/LaunchActivity.java
@@ -526,9 +526,9 @@ potokDrawerView.setOnDrawerItemClickListener(id -> {
         Bundle args = new Bundle();
         args.putInt("type", 0);
         actionBarLayout.presentFragment(new CallLogActivity());
-  } else if (id == org.telegram.ui.PotokDrawerView.ID_SAVED) {
+} else if (id == org.telegram.ui.PotokDrawerView.ID_SAVED) {
     Bundle args = new Bundle();
-    args.putLong("dialog_id", UserConfig.getInstance(UserConfig.selectedAccount).getClientUserId());
+    args.putLong("user_id", UserConfig.getInstance(UserConfig.selectedAccount).getClientUserId());
     AndroidUtilities.runOnUIThread(() -> {
         BaseFragment lastFragment = actionBarLayout.getLastFragment();
         if (lastFragment instanceof MainTabsActivity) {

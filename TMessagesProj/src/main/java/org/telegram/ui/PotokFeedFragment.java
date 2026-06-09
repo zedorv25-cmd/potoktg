9dd24976acf7d5b87053509f4b9467c5be5bd86b
zedorv25-cmd <zedorv25@gmail.com>
2026-06-09 11:49:07 +0500

    Create PotokFeedFragment.java

---
+++ TMessagesProj/src/main/java/org/telegram/ui/PotokFeedFragment.java
@@ -0,0 +1,16 @@
+package org.telegram.ui;
+
+import android.content.Context;
+import android.view.View;
+import android.widget.FrameLayout;
+
+import org.telegram.ui.ActionBar.BaseFragment;
+
+public class PotokFeedFragment extends BaseFragment {
+
+    @Override
+    public View createView(Context context) {
+        fragmentView = new FrameLayout(context);
+        return fragmentView;
+    }
+}

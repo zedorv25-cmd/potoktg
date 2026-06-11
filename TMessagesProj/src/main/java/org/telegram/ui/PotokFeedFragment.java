7ae5a3bd266a154d891b4bd7b0761adcc7ab9798
zedorv25-cmd <zedorv25@gmail.com>
2026-06-11 14:29:19 +0500

    Update PotokFeedFragment.java

--- TMessagesProj/src/main/java/org/telegram/ui/PotokFeedFragment.java
+++ TMessagesProj/src/main/java/org/telegram/ui/PotokFeedFragment.java
@@ -1,13 +1,9 @@
 package org.telegram.ui;
-
 import android.content.Context;
 import android.view.View;
 import android.widget.FrameLayout;
-
 import org.telegram.ui.ActionBar.BaseFragment;
-
-public class PotokFeedFragment extends BaseFragment {
-
+public class PotokFeedFragment extends BaseFragment implements MainTabsActivity.TabFragmentDelegate {
     @Override
     public View createView(Context context) {
         fragmentView = new FrameLayout(context);

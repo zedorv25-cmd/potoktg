a4978934826f9e5f016a3f63bfddcd963ababe80
zedorv25-cmd <zedorv25@gmail.com>
2026-06-12 11:44:06 +0500

    Update PotokFeedFragment.java

--- TMessagesProj/src/main/java/org/telegram/ui/PotokFeedFragment.java
+++ TMessagesProj/src/main/java/org/telegram/ui/PotokFeedFragment.java
@@ -1,12 +1,22 @@
 package org.telegram.ui;
+
 import android.content.Context;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.FrameLayout;
+
 import org.telegram.ui.ActionBar.BaseFragment;
+
 public class PotokFeedFragment extends BaseFragment implements MainTabsActivity.TabFragmentDelegate {
+
     @Override
     public View createView(Context context) {
         fragmentView = new FrameLayout(context);
         return fragmentView;
+    }
+
+    @Override
+    public boolean canParentTabsSlide(MotionEvent ev, boolean forward) {
+        return true;
     }
 }
